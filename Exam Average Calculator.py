ae68bf33f4afbe5b6e7f7793cba72fb80f21a5eb
Ugonna <82211473+AmethystCane@users.noreply.github.com>
2021-04-22 19:40:06 +0100

    Add files via upload

---
+++ Exam Average Calculator.py
@@ -0,0 +1,15 @@
+# Weighted exam score average
+
+# Entering how many exams you have done
+number_of_exams = int(input("Enter number of exams: "))
+
+# Entering how many credits these exams cover
+total_credits = int(input("Enter the total number of credits these exams cover: "))
+
+# Beginning with an average of 0, we add all the percentages from each exam
+average = 0
+for exam in range(number_of_exams):
+    score = int(input("Enter exam score: "))
+    exam_credits = int(input("Enter how many credits this exam covered: "))
+    average = average + score*exam_credits/total_credits
+print("Your average is ", average)
